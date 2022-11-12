85cce56d5c367ef583a54581491637d0b1bba9d0
Dohyun Chung <pastapeter@naver.com>
2022-11-12 17:19:40 +0900

    [add] project Setting

---
+++ KorailTalk/KorailTalk/Tabbar/TabbarViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TabbarViewController.swift
+//  KorailTalk
+//
+//  Created by Jung peter on 11/12/22.
+//
+
+import UIKit
+
+class TabbarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+// TODO: ViewController 4개 연결해야함
+// TODO: 첫번째로 보여질 친구 만들어줘야겠지?
+// TODO: 뷰가 네비게이션인지 아닌지도 파악해줘야
+    
+
+}
