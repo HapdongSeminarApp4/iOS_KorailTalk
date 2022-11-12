908e9190ea4b846887fa1c6c76ee4008c9a5e406
Dohyun Chung <pastapeter@naver.com>
2022-11-12 16:41:51 +0900

    [add] project Setting

---
+++ KorailTalk/KorailTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KorailTalk
+//
+//  Created by Jung peter on 11/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
