0f30d6ae1d1b75a3973f74b4b7cfbdeda641ebc8
정채은 <cce523@hanmail.net>
2022-11-15 12:31:40 +0900

    [add] add assets - images'

--- KorailTalk/KorailTalk/Guide/GuideFirstViewController.swift
+++ KorailTalk/KorailTalk/Guide/GuideFirstViewController.swift
@@ -6,8 +6,28 @@
 //
 
 import UIKit
+import SnapKit
+import SwiftyColor
+import Then
 
 class GuideFirstViewController: UINavigationController {
+    
+    private let guideLabel: UILabel = {
+        let label = UILabel()
+        label.text = "동의 안내"
+        label.font = UIFont(name: "h1", size: 16)
+        label.textColor = 0x0B4199.color
+        return label
+    }()
+    
+    private let titleLabel : UILabel = {
+        let label = UILabel()
+        label.text = "타인 양도 불가"
+        label.font = UIFont(name: "h2", size: 20)
+        label.textColor = 0x0B4199.color
+        return label
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
