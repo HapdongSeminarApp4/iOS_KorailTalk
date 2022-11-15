bfe4b5d763cc282f6f11055c446fa370bc1d71f7
정채은 <cce523@hanmail.net>
2022-11-15 12:16:10 +0900

    [add] add GuideFirstVC

---
+++ KorailTalk/KorailTalk/Guide/GuideFirstViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GuideFirstViewController.swift
+//  KorailTalk
+//
+//  Created by 정채은 on 2022/11/14.
+//
+
+import UIKit
+
+class GuideFirstViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
